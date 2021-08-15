34e9e1d0f3c4b26423ca3d3632e24f024061034e
“Chapman <jc17122@essex.ac.uk>
2021-08-15 13:49:38 +0100

    Added Tests and Fixed bugs

--- src/contracts/RockPaperScissors.sol
+++ src/contracts/RockPaperScissors.sol
@@ -30,6 +30,18 @@ contract RockPaperScissors {
         greedy_contract = GreedyToken(_greedy_address);
     }
 
+    // Events
+    event game_created(bytes32 name, address one, address two);
+    event added_move(bytes32 name, address one);
+    event move_revealed(bytes32 name, address one, uint8 move);
+    event winnings_moved_to_store(address one, uint amount);
+    event winnings_moved_to_address(address one, uint amount);
+
+
+    function get_winnings() view public returns (uint){
+        return winnings[msg.sender];
+    }
+
 
     function create_game(address _playerTwo, bytes32 _name, bytes32 _move, uint _entryFee) public {
         require(games_created[_name].playerOne == address(0x0), "Sorry game name has been taken, game Name must be unique");
@@ -47,6 +59,8 @@ contract RockPaperScissors {
             moveTwo: 0,
             complete: false
         });
+
+        emit game_created(_name, msg.sender, _playerTwo);
     }
 
     function get_game(bytes32 _name) view public returns (address, address, uint) {
@@ -56,30 +70,37 @@ contract RockPaperScissors {
     }
 
     function reveal_move(bytes32 _name, bytes32 _move) public {
-        require(games_created[_name].playerOne != address(0x0), "Game reference does not exist");
-        require(games_created[_name].playerOne == msg.sender || games_created[_name].playerTwo == msg.sender, "Must be one of the two players");
-        require(games_created[_name].moveTwo != 0, "Must allow player two to make a move");
+        require(games_created[_name].playerOne != address(0x0), "Game referenced does not exist");
         require(games_created[_name].complete == false, "Game must not already be claimed");
+        require(games_created[_name].playerOne == msg.sender || games_created[_name].playerTwo == msg.sender, "Must be one of the two players");
+        require(games_created[_name].moveTwoSecret != "", "Must allow player two to make a move");
+        require(move_not_already_placed(_name), "Cannot reveal move again");
 
         if (games_created[_name].playerOne == msg.sender && keccak256(abi.encodePacked(_move, msg.sender)) == games_created[_name].moveOneSecret) {
 
-            if (games_created[_name].moveOne != 0) {
-                revert("Value has already been assigned");
-            }
-            
             games_created[_name].moveOne = convert_to_byte(_move);
+             emit move_revealed(_name, msg.sender, games_created[_name].moveOne);
             
 
-        } else if (games_created[_name].playerOne == msg.sender && keccak256(abi.encodePacked(_move, msg.sender))  == games_created[_name].moveTwoSecret) {
+        } else if (games_created[_name].playerTwo == msg.sender && keccak256(abi.encodePacked(_move, msg.sender))  == games_created[_name].moveTwoSecret) {
+            
+            games_created[_name].moveTwo = convert_to_byte(_move);
+             emit move_revealed(_name, msg.sender, games_created[_name].moveTwo);
 
-            if (games_created[_name].moveTwo != 0) {
-                revert("Value has already been assigned");
-            }
+        } else {
 
-            games_created[_name].moveOne = convert_to_byte(_move);
-
+            revert("unexpected error occurred");
         }
 
+    }
+
+    function move_not_already_placed(bytes32 _name) internal view returns (bool) {
+
+        if (msg.sender == games_created[_name].playerOne) {
+            return (games_created[_name].moveOne  == 0);
+        }
+
+        return (games_created[_name].moveTwo  == 0);
     }
 
     function convert_to_byte(bytes32 _move) internal pure returns (uint8) {
@@ -102,14 +123,16 @@ contract RockPaperScissors {
     }
 
     function add_move(bytes32 _name, bytes32 _move) public {
-        require(games_created[_name].playerOne != address(0x0), "Game reference does not exist");
+        require(games_created[_name].playerOne != address(0x0), "Game referenced does not exist");
         require(games_created[_name].playerTwo == msg.sender, "Only the second player can add a move to the game");
-        require(games_created[_name].moveTwo == 0, "Cannot change move once set");
+        require(games_created[_name].moveTwoSecret == "", "Cannot change move once set");
         require(games_created[_name].complete == false, "Game must not already be claimed");
         require(validWordUsed(_move), "Cannot use the words Rock, Paper, or Scissors, To increase Smart Contract Spying for easy Wins. Use a word that contains one of the follwoing only R's, or only P's or only S's (any case)");
         require(move_winnings(games_created[_name].entryFee) || greedy_contract.transferFrom(msg.sender, address(this), games_created[_name].entryFee), "Token fee failed");
 
         games_created[_name].moveTwoSecret = keccak256(abi.encodePacked(_move, msg.sender));
+
+        emit added_move(_name, msg.sender);
 
     }
 
@@ -119,6 +142,7 @@ contract RockPaperScissors {
         bool found_p = false;
         bool found_s = false;
 
+        // Look for r, s or p
         for (uint i = 0; i < _move.length; i++){
             if (_move[i] == "r" || _move[i] == "R"){
                 found_r = true;
@@ -132,12 +156,13 @@ contract RockPaperScissors {
             }
         }
 
+        // only contains one of the r s p 
         return ((found_r || found_s || found_p) && !((found_r && found_p)|| (found_r && found_s) || (found_p && found_s)));
 
     }
 
     function claim_winnings(bytes32 _name, bool _transferred_out) public {
-        require(games_created[_name].playerOne != address(0x0), "Game reference does not exist");
+        require(games_created[_name].playerOne != address(0x0), "Game referenced does not exist");
         require(games_created[_name].complete == false, "Game must not already be claimed");
         require(((games_created[_name].moveOne != 0 && games_created[_name].moveTwo != 0 ) && has_won_or_drawn(_name)) || timeout_fault(_name), "Only winner can claim, or time out for a player who has revealed");
         
@@ -149,24 +174,34 @@ contract RockPaperScissors {
             // if drawn return money back to the other player
             if (msg.sender == games_created[_name].playerOne){
                 winnings[games_created[_name].playerTwo] = games_created[_name].entryFee;
+                emit winnings_moved_to_store(games_created[_name].playerTwo, games_created[_name].entryFee);
             } else {
                 winnings[games_created[_name].playerOne] = games_created[_name].entryFee;
+                emit winnings_moved_to_store(games_created[_name].playerOne, games_created[_name].entryFee);
             }
            
             // Handle own tokens
             if (_transferred_out) {
-                greedy_contract.transferFrom(msg.sender, address(this), games_created[_name].entryFee);
+                greedy_contract.approve(address(this), games_created[_name].entryFee);
+                greedy_contract.transferFrom(address(this), msg.sender, games_created[_name].entryFee);
+
+                emit winnings_moved_to_address(games_created[_name].playerOne, games_created[_name].entryFee);
             } else {
+
                 winnings[msg.sender] = games_created[_name].entryFee;
+                emit winnings_moved_to_store(games_created[_name].playerOne, games_created[_name].entryFee);
             }
 
         
         } else {
             // Handle own winning tokens
             if (_transferred_out) {
-                greedy_contract.transferFrom(msg.sender, address(this), games_created[_name].entryFee * 2);
+                greedy_contract.approve(address(this), games_created[_name].entryFee * 2);
+                greedy_contract.transferFrom(address(this), msg.sender, games_created[_name].entryFee * 2);
+                emit winnings_moved_to_address(games_created[_name].playerOne, games_created[_name].entryFee * 2);
             } else {
                 winnings[msg.sender] = games_created[_name].entryFee * 2;
+                emit winnings_moved_to_store(games_created[_name].playerOne, games_created[_name].entryFee);
             }
         }
 
@@ -180,10 +215,9 @@ contract RockPaperScissors {
         if (games_created[_name].timestamp + 30 < block.timestamp) {
 
             // player two has ignored the game -> player one should get tokens back
-            if (games_created[_name].moveTwoSecret == "" ) {
+            if (games_created[_name].moveTwoSecret == "" && msg.sender == games_created[_name].playerOne) {
                 return true;
             }
-
             
             if (msg.sender == games_created[_name].playerOne) {
 
@@ -230,16 +264,13 @@ contract RockPaperScissors {
         - 2 is scissors
         - 3 is paper
         */
-
         //Rock and Scissors - Player One
         if (games_created[_name].moveOne ==  1 && games_created[_name].moveTwo == 2 && msg.sender == games_created[_name].playerOne){
-            
             return true;
         }
 
         //Rock and Scissors - Player two
         if (games_created[_name].moveOne ==  2 && games_created[_name].moveTwo == 1 && msg.sender == games_created[_name].playerTwo){
-            
             return true;
         }
 
@@ -270,6 +301,21 @@ contract RockPaperScissors {
 
         // Did not win
         return false;
+
+    }
+
+
+    function withdraw_winnings() public {
+        require(winnings[msg.sender] != 0, "Have not winnings to withdraw");
+
+        greedy_contract.approve(address(this), winnings[msg.sender]);
+        greedy_contract.transferFrom(address(this), msg.sender, winnings[msg.sender]);
+
+        uint amount_sent = winnings[msg.sender];
+
+        winnings[msg.sender] = 0;
+
+        emit winnings_moved_to_address(msg.sender, amount_sent);
 
     }
 
--- src/test/RockPaperScissors.js
+++ src/test/RockPaperScissors.js
@@ -1,14 +1,15 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Game contract", () => {
     
-    let owner, playerone, playertwo;
+    let owner, playerone, playertwo, playerthree;
 
     let greedyToken, game;
     
     before('Gets data before test cases', async () => {
         // Create Signers
-        [owner, playerone, playertwo] = await ethers.getSigners();
+        [owner, playerone, playertwo, playerthree] = await ethers.getSigners();
 
         // Deploy Tokens
         const Token = await ethers.getContractFactory("GreedyToken");
@@ -26,7 +27,7 @@ describe("Game contract", () => {
         
     })
 
-    it("Can create Game", async () => {
+    it("Can create Game with Valid Move", async () => {
 
         // Create Game
         await greedyToken.connect(playerone).approve(game.address, 5);
@@ -37,6 +38,331 @@ describe("Game contract", () => {
         expect(game_details[0]).to.equal(playerone.address);
         expect(game_details[1]).to.equal(playertwo.address);
         expect(game_details[2]).to.equal(5);
+
+    });
+
+    it("Cannot create Game with same name", async () => {
+
+        // Create Game again 
+        await greedyToken.connect(playerone).approve(game.address, 5);
+        try {
+            await game.connect(playerone).create_game(playertwo.address, ethers.utils.formatBytes32String('test'), ethers.utils.formatBytes32String('rhino'), 5);
+
+            assert.fail(0, 1, 'Exception not thrown');
+            
+        }catch(err){
+            expect(err.message).to.equal("VM Exception while processing transaction: reverted with reason string 'Sorry game name has been taken, game Name must be unique'");
+        }
+            
+
+    });
+
+    
+    it("Invalid Move Used", async () => {
+
+        // With no r p or s
+        try {
+            await game.connect(playerone).create_game(playertwo.address, ethers.utils.formatBytes32String('test_one'), ethers.utils.formatBytes32String('a'), 5);
+
+            assert.fail(0, 1, 'Exception not thrown');
+            
+        }catch(err){
+            expect(err.message).to.equal("VM Exception while processing transaction: reverted with reason string 'Cannot use the words Rock, Paper, or Scissors, To increase Smart Contract Spying for easy Wins. Use a word that contains only R's, or only P's or only S's (any case)'");
+        }
+
+        // With no r p or s
+        try {
+            await game.connect(playerone).create_game(playertwo.address, ethers.utils.formatBytes32String('test_one'), ethers.utils.formatBytes32String('rps'), 5);
+
+            assert.fail(0, 1, 'Exception not thrown');
+            
+        }catch(err){
+            expect(err.message).to.equal("VM Exception while processing transaction: reverted with reason string 'Cannot use the words Rock, Paper, or Scissors, To increase Smart Contract Spying for easy Wins. Use a word that contains only R's, or only P's or only S's (any case)'");
+        }
+
+        // Blank Word
+        try {
+            await game.connect(playerone).create_game(playertwo.address, ethers.utils.formatBytes32String('test_one'), ethers.utils.formatBytes32String(''), 5);
+
+            assert.fail(0, 1, 'Exception not thrown');
+            
+        }catch(err){
+            expect(err.message).to.equal("VM Exception while processing transaction: reverted with reason string 'Cannot use the words Rock, Paper, or Scissors, To increase Smart Contract Spying for easy Wins. Use a word that contains only R's, or only P's or only S's (any case)'");
+        }
+    });
+
+    it("Cannot create Game with not enough funds without any winnings", async () => {
+
+        // Create Game again 
+        await greedyToken.connect(playerone).approve(game.address, 5);
+        try {
+            // Only approved 5 so far
+            await game.connect(playerone).create_game(playertwo.address, ethers.utils.formatBytes32String('test_one'), ethers.utils.formatBytes32String('s'), 10);
+
+            assert.fail(0, 1, 'Exception not thrown');
+            
+        }catch(err){
+            expect(err.message).to.equal("VM Exception while processing transaction: reverted with reason string 'ERC20: transfer amount exceeds allowance'");
+        }
+
+    });
+
+    it("Add invalid move to valid Game", async () => {
+
+        // Create Game again 
+        await greedyToken.connect(playertwo).approve(game.address, 5);
+        try {
+            // Only approved 5 so far
+            await game.connect(playertwo).add_move(ethers.utils.formatBytes32String('test'), ethers.utils.formatBytes32String(''));
+            
+            assert.fail(0, 1, 'Exception not thrown');
+            
+        }catch(err){
+            expect(err.message).to.equal("VM Exception while processing transaction: reverted with reason string 'Cannot use the words Rock, Paper, or Scissors, To increase Smart Contract Spying for easy Wins. Use a word that contains one of the follwoing only R's, or only P's or only S's (any case)'");
+        }
+
+    });
+
+    it("Add invalid move to valid Game", async () => {
+
+        // Create Game again 
+        await greedyToken.connect(playertwo).approve(game.address, 0);
+        try {
+            // Only approved 5 so far
+            await game.connect(playertwo).add_move(ethers.utils.formatBytes32String('test'), ethers.utils.formatBytes32String('s'));
+            
+            assert.fail(0, 1, 'Exception not thrown');
+            
+        }catch(err){
+            expect(err.message).to.equal("VM Exception while processing transaction: reverted with reason string 'ERC20: transfer amount exceeds allowance'");
+        }
+
+    });
+
+    it("Add move to valid Game", async () => {
+
+        // Create Game again 
+        await greedyToken.connect(playertwo).approve(game.address, 5);
+        try {
+            // Only approved 5 so far
+            await game.connect(playertwo).add_move(ethers.utils.formatBytes32String('test'), ethers.utils.formatBytes32String('s'));
+            
+        }catch(err){
+
+            
+            assert.fail(0, 1, 'Exception not thrown');
+        }
+
+    });
+
+    it("Add move to valid Game again", async () => {
+
+        // Create Game again 
+        await greedyToken.connect(playertwo).approve(game.address, 5);
+        try {
+            // Only approved 5 so far
+            await game.connect(playertwo).add_move(ethers.utils.formatBytes32String('test'), ethers.utils.formatBytes32String('s'));
+
+            assert.fail(0, 1, 'Exception not thrown');
+            
+        }catch(err){
+            expect(err.message).to.equal("VM Exception while processing transaction: reverted with reason string 'Cannot change move once set'");
+        }
+
+    });
+
+    it("Add move to non-existant Game", async () => {
+
+        // Create Game again 
+        await greedyToken.connect(playertwo).approve(game.address, 5);
+        try {
+            // Only approved 5 so far
+            await game.connect(playertwo).add_move(ethers.utils.formatBytes32String('test_one'), ethers.utils.formatBytes32String('s'));
+
+            assert.fail(0, 1, 'Exception not thrown');
+            
+        }catch(err){
+
+            expect(err.message).to.equal("VM Exception while processing transaction: reverted with reason string 'Game referenced does not exist'");
+
+        }
+
+    });
+
+    it("Add move as player one to a Game", async () => {
+
+        // Create Game again 
+        await greedyToken.connect(playerone).approve(game.address, 5);
+        try {
+            // Only approved 5 so far
+            await game.connect(playerone).add_move(ethers.utils.formatBytes32String('test'), ethers.utils.formatBytes32String('s'));
+
+            assert.fail(0, 1, 'Exception not thrown');
+            
+        }catch(err){
+
+            expect(err.message).to.equal("VM Exception while processing transaction: reverted with reason string 'Only the second player can add a move to the game'");
+
+        }
+
+    });
+
+    it("Reveal Move on game that does not exist", async () => {
+
+        try {
+            await game.connect(playerone).reveal_move(ethers.utils.formatBytes32String('test_one'), ethers.utils.formatBytes32String('rhino'));
+
+            assert.fail(0, 1, 'Exception not thrown');
+            
+        }catch(err){
+
+            expect(err.message).to.equal("VM Exception while processing transaction: reverted with reason string 'Game referenced does not exist'");
+
+        }
+
+    });
+
+    it("Reveal Move on game with player not in the game", async () => {
+
+        try {
+            await game.connect(playerthree).reveal_move(ethers.utils.formatBytes32String('test'), ethers.utils.formatBytes32String('rhino'));
+
+            assert.fail(0, 1, 'Exception not thrown');
+            
+        }catch(err){
+
+            expect(err.message).to.equal("VM Exception while processing transaction: reverted with reason string 'Must be one of the two players'");
+
+        }
+
+    });
+
+    it("Reveal Move on game where playertwo has not had a go", async () => {
+
+        await greedyToken.connect(playerone).approve(game.address, 5);
+        await game.connect(playerone).create_game(playertwo.address, ethers.utils.formatBytes32String('test_two'), ethers.utils.formatBytes32String('rhino'), 5);
+
+        try {
+
+            await game.connect(playerone).reveal_move(ethers.utils.formatBytes32String('test_two'), ethers.utils.formatBytes32String('rhino'));
+
+            assert.fail(0, 1, 'Exception not thrown');
+            
+        }catch(err){
+
+            expect(err.message).to.equal("VM Exception while processing transaction: reverted with reason string 'Must allow player two to make a move'");
+
+        }
+
+    });
+
+    it("Reveal Both Moves on game where playerone and playertwo have placed", async () => {
+
+        await greedyToken.connect(playerone).approve(game.address, 5);
+        await greedyToken.connect(playertwo).approve(game.address, 5);
+
+        await game.connect(playerone).create_game(playertwo.address, ethers.utils.formatBytes32String('test_three'), ethers.utils.formatBytes32String('rhino'), 5);
+        await game.connect(playertwo).add_move(ethers.utils.formatBytes32String('test_three'), ethers.utils.formatBytes32String('s'));
+
+        try {
+            
+            await game.connect(playerone).reveal_move(ethers.utils.formatBytes32String('test_three'), ethers.utils.formatBytes32String('rhino'));
+            await game.connect(playertwo).reveal_move(ethers.utils.formatBytes32String('test_three'), ethers.utils.formatBytes32String('s'));
+
+        }catch(err){
+            assert.fail(0, 1, 'Exception not thrown');
+
+        }
+
+    });
+
+
+    it("Claim winnings for game that does not exist", async () => {
+
+        try {
+            
+            await game.connect(playerone).claim_winnings(ethers.utils.formatBytes32String('test_four'), false);
+
+            assert.fail(0, 1, 'Exception not thrown');
+
+        }catch(err){           
+
+            expect(err.message).to.equal("VM Exception while processing transaction: reverted with reason string 'Game referenced does not exist'");
+
+        }
+
+    });
+
+    it("Claim winnings with loser", async () => {
+
+        try {
+            
+            await game.connect(playertwo).claim_winnings(ethers.utils.formatBytes32String('test_three'), false);
+
+            assert.fail(0, 1, 'Exception not thrown');
+
+        }catch(err){           
+
+            expect(err.message).to.equal("VM Exception while processing transaction: reverted with reason string 'Only winner can claim, or time out for a player who has revealed'");
+
+        }
+
+    });
+
+    it("Claim winnings with winner with no transfer out", async () => {
+
+        try {
+            const pre_winnings = await game.connect(playerone).get_winnings();
+
+            expect(pre_winnings).to.equal(0);
+
+            await game.connect(playerone).claim_winnings(ethers.utils.formatBytes32String('test_three'), false);
+
+            const post_winnings = await game.connect(playerone).get_winnings();
+
+            expect(post_winnings).to.equal(10);
+
+        }catch(err){           
+            console.log(err.message)
+            assert.fail(0, 1, 'Exception not thrown');
+
+        }
+
+    });
+
+    it("Transfer stored winnings", async () => {
+
+        try {
+            const post_winnings = await game.connect(playerone).get_winnings();
+            expect(post_winnings).to.equal("10");
+
+            await game.connect(playerone).withdraw_winnings();
+            const post_with = await game.connect(playerone).get_winnings();
+            expect(post_with).to.equal(0);
+
+            expect(await greedyToken.balanceOf(playerone.address)).to.equal(45);
+
+        }catch(err){           
+            assert.fail(0, 1, 'Exception not thrown');
+
+        }
+
+    });
+
+    it("Claim winnings with winner with transfer out", async () => {
+
+        await greedyToken.connect(playerone).approve(game.address, 10);
+        await greedyToken.connect(playertwo).approve(game.address, 10);
+
+        await game.connect(playerone).create_game(playertwo.address, ethers.utils.formatBytes32String('test_transfer'), ethers.utils.formatBytes32String('s'), 10);
+        await game.connect(playertwo).add_move(ethers.utils.formatBytes32String('test_transfer'), ethers.utils.formatBytes32String('p'));
+
+        await game.connect(playerone).reveal_move(ethers.utils.formatBytes32String('test_transfer'), ethers.utils.formatBytes32String('s'));
+        await game.connect(playertwo).reveal_move(ethers.utils.formatBytes32String('test_transfer'), ethers.utils.formatBytes32String('p'));
+
+        await game.connect(playerone).claim_winnings(ethers.utils.formatBytes32String('test_transfer'), true);
+
+        expect(await greedyToken.balanceOf(playerone.address)).to.equal(55);
 
     });
 
